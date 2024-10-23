00246858216251e65014bb3aee4851afd50a5497
MohammadAlaa-Alghamry <mohammad.alaa.alghamry@gmail.com>
2024-10-23 09:34:57 +0300

    Created first Vulkan instance.

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -21,11 +22,12 @@ private:
 	GLFWwindow* window;
 	GLFWmonitor* monitorPrimary;
 	GLFWmonitor** monitors;
+	VkInstance instance;
 
 	void initWindow() {
 		glfwInit();
 
-		std::cout << "Initializing my first GLFW window" << std::endl;
+		std::cout << "Initializing my first GLFW window\n";
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
@@ -38,6 +40,54 @@ private:
 	}
 
 	void initVulkan() {
+		createInstance();
+	}
+
+	void createInstance() {
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+		
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+		std::cout << "available extensions:\n";
+		for (const auto& extension : extensions) {
+			std::cout << '\t' << extension.extensionName << '\n';
+		}
+		std::cout << "required glfw extensions:\n";
+		for (int i=0; i < glfwExtensionCount; i++) {
+			std::cout << '\t' << glfwExtensions[i] << '\n';
+		}
+
+
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}
+
+		std::cout << "Done initialising Volkan instance.\n";
+
 
 	}
 
