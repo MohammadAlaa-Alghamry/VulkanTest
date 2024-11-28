92f4ca630f97a98d1ea413c3762c8fc92931c68d
MohammadAlaa-Alghamry <mohammad.alaa.alghamry@gmail.com>
2024-11-28 05:05:06 +0200

    Render Pass Done.

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -103,6 +103,7 @@ private:
 	VkFormat swapChainImageformat;
 	VkExtent2D swapChainExtent;
 
+	VkRenderPass renderPass;
 	VkPipelineLayout pipelineLayout;
 
 	std::vector<VkImageView> swapChainImageViews;
@@ -127,6 +128,7 @@ private:
 		createLogicalDevice();
 		createSwapChain();
 		createImageViews();
+		createRenderPass();
 		createGraphicsPipeline();
 	}
 
@@ -142,6 +144,7 @@ private:
 		}
 
 		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+		vkDestroyRenderPass(device, renderPass, nullptr);
 
 		for (auto imageView : swapChainImageViews) { vkDestroyImageView(device, imageView, nullptr); }
 		vkDestroySwapchainKHR(device, swapChain, nullptr);
@@ -603,8 +606,41 @@ private:
 				throw std::runtime_error("failed to create image views!");
 			}
 		}
-
 	}
+
+	void createRenderPass() {
+		VkAttachmentDescription colorAttachment{};
+		colorAttachment.format = swapChainImageformat;
+		colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+		colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+		colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+		colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+		colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+		colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+		VkAttachmentReference colorAttachmentRef{};
+		colorAttachmentRef.attachment = 0;
+		colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+		VkSubpassDescription subpass{};
+		subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+		subpass.colorAttachmentCount = 1;
+		subpass.pColorAttachments = &colorAttachmentRef;
+
+		VkRenderPassCreateInfo renderPassInfo{};
+		renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+		renderPassInfo.attachmentCount = 1;
+		renderPassInfo.pAttachments = &colorAttachment;
+		renderPassInfo.subpassCount = 1;
+		renderPassInfo.pSubpasses = &subpass;
+
+		if (vkCreateRenderPass(device, &renderPassInfo, nullptr, &renderPass)) {
+			throw std::runtime_error("failed to create render passs!");
+		}
+	}
+
+
 
 	void createGraphicsPipeline() {
 		auto vertShaderCode = readFile("shaders/vert.spv");
@@ -627,6 +663,8 @@ private:
 		fragShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
 		fragShaderStageInfo.module = fragShaderModule;
 		fragShaderStageInfo.pName = "main";
+
+
 
 		VkPipelineShaderStageCreateInfo shaderStages[] = { vertShaderStageInfo, fragShaderStageInfo };
 
